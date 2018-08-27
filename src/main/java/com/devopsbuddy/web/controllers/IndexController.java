23bb0dfe96e1262545ac6de568309df7f424406e
Francisco Rodríguez <francisco.rodriguez0027@protonmail.com>
2018-08-27 15:25:09 -0500

    Added placeholder

---
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
